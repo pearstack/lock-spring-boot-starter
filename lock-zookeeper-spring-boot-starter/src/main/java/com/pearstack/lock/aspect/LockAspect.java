cf62ca45f6c388a07df0b14f19b17eeb40a5cff4
lihao3 <lihao3@wison.com>
2021-12-17 14:41:48 +0800

    调整目录结构

--- lock-zookeeper-spring-boot-starter/src/main/java/com/pearstack/lock/aspect/LockAspect.java
+++ lock-zookeeper-spring-boot-starter/src/main/java/com/pearstack/lock/aspect/LockAspect.java
@@ -3,7 +3,6 @@ package com.pearstack.lock.aspect;
 import com.pearstack.lock.annotation.Locked;
 import com.pearstack.lock.service.LockFailedService;
 import com.pearstack.lock.service.LockKeyService;
-import com.pearstack.lock.spring.boot.autoconfigure.LockAutoProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -13,6 +12,7 @@ import org.springframework.integration.zookeeper.lock.ZookeeperLockRegistry;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
 /**
@@ -24,7 +24,6 @@ import java.util.concurrent.locks.Lock;
 @Component
 public class LockAspect {
 
-  @Resource private LockAutoProperties properties;
   @Resource private ZookeeperLockRegistry zookeeperLockRegistry;
   @Resource private LockKeyService lockKeyService;
   @Resource private LockFailedService lockFailedService;
@@ -38,24 +37,41 @@ public class LockAspect {
   public void lockPointCut(Locked locked) {}
 
   @Around(value = "lockPointCut(locked)", argNames = "joinPoint,locked")
-  public Object around(ProceedingJoinPoint joinPoint, Locked locked) throws Throwable {
-    // 判断是否开启
+  public Object around(ProceedingJoinPoint joinPoint, Locked locked) {
+    // 防止重试时间大于超时时间
+    if (locked.retryInterval() >= locked.acquireTimeout()) {
+      log.warn("retryInterval more than acquireTimeout,please check your configuration");
+    }
     // 获取分布式锁的key
-    String key =
-        properties.getPrefix()
-            + ":"
-            + lockKeyService.getKey(joinPoint, locked.name(), locked.keys());
+    String key = lockKeyService.getKey(joinPoint, locked.name(), locked.keys());
     // 初始化锁对象
     Lock lock = zookeeperLockRegistry.obtain(key);
-    // 尝试上锁
-    boolean lockFlag = lock.tryLock(properties.getExpire(), properties.getUnit());
-
     Object result = null;
+    // 初始化上锁是否成功标识
+    boolean lockFlag;
     try {
+      // 尝试上锁
+      lockFlag = lock.tryLock(locked.expire(), locked.unit());
+      // 判断是否上锁成功
+      if (!lockFlag) {
+        // 获取当前时间
+        long start = System.currentTimeMillis();
+        // 循环遍历上锁
+        while (System.currentTimeMillis() - start < locked.acquireTimeout()) {
+          lockFlag = lock.tryLock(locked.expire(), locked.unit());
+          TimeUnit.MILLISECONDS.sleep(locked.retryInterval());
+        }
+      }
+      // 如果最后还是上锁失败, 那就执行异常方法
+      if (!lockFlag) {
+        lockFailedService.onLockFailed(key);
+      }
+      // 开始执行业务代码
       result = joinPoint.proceed();
     } catch (Throwable e) {
       lockFailedService.onLockFailed(key);
     } finally {
+      // 解锁
       lock.unlock();
     }
     return result;
